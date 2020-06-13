dbe69ad4af4fa9081fe243ce737a166c05df8dd9
Chris hsiao <chris.hsiao@igroup.tw>
2020-06-13 14:12:42 +0800

    [新增] 更新eslint規則，更新App.js內容格式

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
-import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.props = props;
-  }
+    constructor(props) {
+        super(props);
+        this.props = props;
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <h1> Hello, World! </h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1> Hello, World! </h1>
+            </div>
+        );
+    }
 }
 
 export default hot(module)(App);
